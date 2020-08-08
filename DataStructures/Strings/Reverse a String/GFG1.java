e2c85e92b1729e30d94b3803bbb1dbeef69ca607
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-08-08 19:04:35 +0530

    Create GFG1.java

---
+++ DataStructures/Strings/Reverse a String/GFG1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class GFG1 {
+	public static void main (String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    int t = scan.nextInt();
+	    while(t-- >0){
+	        String s = scan.next();
+       
+	        String str ="";
+	        for(int i=s.length()-1;i>=0;i--){
+	            str = str+s.charAt(i);
+	        }
+	        System.out.println(str);
+	    }
+	}
+}
