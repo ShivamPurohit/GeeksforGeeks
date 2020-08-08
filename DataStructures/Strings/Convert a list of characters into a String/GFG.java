83375194c06b454847ae6cca36dd1f1450c8ff54
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-08-08 17:39:24 +0530

    Create GFG.java

---
+++ DataStructures/Strings/Convert a list of characters into a String/GFG.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class GFG {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		while(t-- >0){
+		    int n = scan.nextInt();
+		    char[] ch = new char[n];
+		    for(int i=0;i<n;i++){
+		        ch[i] = scan.next().charAt(0);
+		    }
+		    StringBuilder s = new StringBuilder();
+		    for(Character c : ch){
+		        s.append(c);
+		    }
+		    System.out.println(s);
+		}
+	}
+}
