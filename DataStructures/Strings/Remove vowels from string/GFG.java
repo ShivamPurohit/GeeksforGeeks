55c715c50c2f95d5785cc5cecee95a4c957f5dfc
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-08-09 12:20:14 +0530

    Create GFG.java

---
+++ DataStructures/Strings/Remove vowels from string/GFG.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class GFG {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		scan.nextLine();
+		while(t-- >0){
+		    String s = scan.nextLine();
+		  
+		  System.out.println(remVowel(s));
+		  
+		}
+	}
+	
+	static String remVowel(String str) 
+    { 
+         Character vowels[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'}; 
+           
+         List<Character> al = Arrays.asList(vowels); 
+           
+         StringBuffer sb = new StringBuffer(str); 
+           
+         for (int i = 0; i < sb.length(); i++) { 
+              
+             if(al.contains(sb.charAt(i))){ 
+                sb.replace(i, i+1, "") ; 
+                i--; 
+             } 
+        } 
+           
+          
+        return sb.toString(); 
+    } 
+}
