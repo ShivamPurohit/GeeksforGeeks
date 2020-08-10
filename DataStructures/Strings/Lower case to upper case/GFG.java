e7ea3cf5a8a05a7e150ddfd6e705311c19f2487d
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-08-10 22:41:59 +0530

    Create GFG.java

---
+++ DataStructures/Strings/Lower case to upper case/GFG.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class GFG {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		while(t-- >0){
+		    String s = scan.next();
+		    System.out.println(s.toUpperCase());
+		}
+	}
+}
