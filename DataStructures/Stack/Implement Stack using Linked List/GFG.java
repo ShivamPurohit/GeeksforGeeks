9a28e0dfaafd7c236a4c3f2941efde8ef5997923
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-09-11 16:28:45 +0530

    Create GFG.java

---
+++ DataStructures/Stack/Implement Stack using Linked List/GFG.java
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+import java.util.*;
+
+class StackNode {
+    int data;
+    StackNode next;
+    StackNode(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+public class GfG {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            MyStack obj = new MyStack();
+            int Q = sc.nextInt();
+            while (Q > 0) {
+                int QueryType = 0;
+                QueryType = sc.nextInt();
+                if (QueryType == 1) {
+                    int a = sc.nextInt();
+                    obj.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(obj.pop() + " ");
+                }
+                Q--;
+            }
+            System.out.println("");
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+The structure of the node of the stack is
+class StackNode
+{
+    int data;
+    StackNode next;
+    StackNode(int a)
+    {
+       data = a;
+       next = null;
+    }
+}
+*/
+class MyStack {
+
+    // Note that top is by default null
+    // in Java
+    StackNode top;
+
+    void push(int a) {
+       StackNode new_node = new StackNode(a);
+       if(top == null){
+           top = new_node;
+       }
+       else{
+           StackNode temp = top;
+           top = new_node;
+           new_node.next = temp;
+       }
+    }
+    int pop() {
+        int popped = Integer.MIN_VALUE;
+        if(top == null){
+            return -1;
+        }
+        else{
+            popped = top.data;
+            top = top.next;
+        }
+        return popped;
+    }
+}
