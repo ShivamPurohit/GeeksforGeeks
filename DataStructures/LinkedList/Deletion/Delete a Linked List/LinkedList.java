399cf35981fb7771f200eddeed7ac5ef3544a67a
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-11-19 09:31:14 +0530

    Create LinkedList.java

---
+++ DataStructures/LinkedList/Deletion/Delete a Linked List/LinkedList.java
@@ -0,0 +1,52 @@
+public class LinkedList{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    
+    void deleteList(){
+        head = null;
+        System.out.println("List is Deleted!");
+    }
+    
+    public void push(int new_data){
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+    
+    public void printList(){
+        Node n = head;
+        while(n != null){
+            System.out.print(n.data + " ");
+            n=n.next;
+        }
+        System.out.println("");
+    }
+    
+    public static void main(String args[]){
+        LinkedList llist = new LinkedList();
+       
+        llist.push(3);
+        llist.push(4);
+        llist.push(1);
+        llist.push(9);
+        llist.push(5);
+        System.out.println("\n Linked List created");
+        llist.printList();
+        
+        llist.deleteList();
+        llist.printList();
+    }
+}
+
+
+//Output :
+// Linked List created
+//5 9 1 4 3 
+//List is Deleted!
