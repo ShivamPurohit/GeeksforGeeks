be750766bd7e0729eb4d5f184a95904a9186817c
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2020-11-21 19:50:19 +0530

    Create Main.java

---
+++ DataStructures/DoublyLinkedList/InsertAfter/Main.java
@@ -0,0 +1,69 @@
+public class Main{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node prev;
+        Node(int d){
+            data = d;
+        }
+        
+    }
+    
+    public void push(int new_data){
+       Node new_node = new Node(new_data);
+       new_node.next = head;
+       new_node.prev = null;
+       
+       if(head!=null){
+           head.prev = new_node;
+       }
+       
+       head = new_node;
+    }
+    
+    public void insertAfter(Node prev_node,int new_data){
+        Node new_node = new Node(new_data);
+        new_node.next = prev_node.next;
+        prev_node.next = new_node;
+        new_node.prev = prev_node;
+        if(new_node.next!=null)
+            new_node.next.prev = new_node;
+    }
+    
+    public void printList(Node node){
+        Node last = node;
+        System.out.println("Traversal in forward Direction \n");
+        while(node!=null){
+            System.out.print(node.data + " ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("");
+        System.out.println("\nTraversal in reverse direction\n");
+        while(last!=null){
+            System.out.print(last.data + " ");
+            last = last.prev;
+        }
+    }
+    
+    public static void main(String args[]){
+        Main dll = new Main();
+        dll.push(1);
+        dll.push(2);
+        dll.push(3);
+        dll.push(4);
+        dll.push(5);
+        dll.insertAfter(dll.head.next,8);
+        dll.printList(dll.head);
+    }
+}
+
+//Output : 
+//Traversal in forward Direction 
+
+//5 4 8 3 2 1 
+
+//Traversal in reverse direction
+
+//1 2 3 8 4 5 
